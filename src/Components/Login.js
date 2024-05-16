47252fe03245a3c8c7c3d3f52fd342b9bfb1a75c
Mursal Furqan <mursalfurqan@gmail.com>
2024-05-16 08:17:34 +0200

    Starting working on Firebase after this

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../styles/style.css";
 import NoUserNavbar from './NoUserNavbar';
+import { Link } from 'react-router-dom';
 
 function Login() {
     return (
@@ -10,15 +11,14 @@ function Login() {
                 <form className="login-form">
                     <h2>Login</h2>
                     <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="username" />
+                        <label htmlFor="username">Email</label>
+                        <input type="email" id="email" name="email" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" name="password" />
                     </div>
-                    {/* Don't have an account */}
-                    <p className='newUser'>Don't have an account? <a href="#">Sign up</a></p>
+                    <p className='newUser'>Don't have an account? <Link to="/register">Sign up.</Link></p>
                     <button type="submit" className="login-btn">Login</button>
                 </form>
             </div>
--- src/Components/Register.js
+++ src/Components/Register.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import "../styles/style.css";
 import NoUserNavbar from './NoUserNavbar';
+import { Link } from 'react-router-dom';
 
 function Register() {
     return (
@@ -10,16 +11,24 @@ function Register() {
                 <form className="register-form">
                     <h2>Register</h2>
                     <div className="form-group">
-                        <label htmlFor="username">Username</label>
-                        <input type="text" id="username" name="username" />
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
                         <input type="password" id="password" name="password" />
                     </div>
+                    <div className="form-group">
+                        <label htmlFor="confirm-password">Confirm Password</label>
+                        <input type="password" id="confirm-password" name="confirm-password" />
+                    </div>
                     {/* Don't have an account */}
-                    <p className='newUser'>Don't have an account? <a href="#">Sign up</a></p>
-                    <button type="submit" className="register-btn">Login</button>
+                    <p className='newUser'>Already have an account? <Link to="/login">Login</Link></p>
+                    <button type="submit" className="register-btn">Register</button>
                 </form>
             </div>
         </>
